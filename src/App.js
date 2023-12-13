517ba0ac616ff091ed542e13bc33d3de48501737
ankitkadyan06 <ankit.kadyan@designx.in>
2023-12-13 22:27:03 +0530

    updated some static data and HT on OFF color

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import HomePage from './Pages/HomePage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-    <HomePage/>
-    </div>
+    <BrowserRouter>
+    <Routes>
+    <Route exact path="/" element={<HomePage />} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
