f0a04adbe4a563da9645544c89b5a191ebf0b07c
ankitkadyan06 <ankit.kadyan@designx.in>
2023-12-13 12:02:43 +0530

    add data

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Tanks from '../Components/Tanks/Tanks'
+
+const HomePage = () => {
+  return (
+    <div>
+      <Tanks/>
+    </div>
+  )
+}
+
+export default HomePage
